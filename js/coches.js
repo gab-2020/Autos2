2f939ec7a9f3cddeeee171ccbce2ffc6ccc0553a
gab-2020 <gabriela.esteban.din@gmail.com>
2020-05-20 04:52:14 -0300

    corrección de github, ahora es público

---
+++ js/coches.js
@@ -0,0 +1,32 @@
+var nombreusuario = nombre (prompt('Escribe tu nombre'));
+
+document.write(`nombreusuario = ${nombreusuario}`);
+
+var coches = nombre (prompt('Nombra tres vehículos que has tenido'));
+function marcascoches (coches){
+  console.log (`Los vehículos que he tenido ${coches}`);
+    }
+    for (var) i=0; i<coches.lenght; i++ {
+      marcascoches (coches [i]);
+
+    }
+
+var marcacoches = nombre (prompt('Marca del coche'));
+
+document.write(`marcacoches = ${marcacoches}`);
+
+var anyo = nombre (prompt('Anyo del coche'));
+
+document.write(`anyo = ${anyo}`);
+
+var color = nombre (prompt('Color del coche'));
+
+document.write(`color= ${color}`);
+
+var puertas = nombre (prompt('Cantidad de puertas'));
+
+document.write(`puertas= ${puertas}`);
+
+
+
+var nombrecoches= ['Lamborghini', 'Bmw', 'Aston Martin']
